1c318dee04b313ec51f62f9b2c488a11b52703fc
Roland <roland.onofrej@akademiasovy.sk>
2018-11-23 09:29:46 +0100

    added new functionality

--- TicTacToe/js/script.js
+++ TicTacToe/js/script.js
@@ -1,9 +1,70 @@
 let board=[0,0,0,0,0,0,0,0,0];
 let player=1;
+let winner=0;
 
 $(document).ready(function() {
 
 });
 function imgClick(index){
   console.log(index);
+  if(board[index]===0 && winner===0){
+    board[index]=player;
+
+    if(player===1)
+        paintCell('img/x.png',index);
+    else
+        paintCell('img/o.png',index);
+    changePlayer();
+    setTimeout(checkWinner,500);
+  }
+}
+
+function paintCell(imagexo,index){
+  let image=document.getElementsByName('image')[index].src=imagexo;
+
+}
+
+function changePlayer(){
+  player=player===1?2:1;
+  let playerDiv=document.getElementById('player');
+  if(player===1)
+    playerDiv.innerHTML='Next player: X';
+   else
+      playerDiv.innerHTML='Next player: O';
+
+}
+
+function checkWinner(){
+  let t =0;
+  for(i=0;i<7;i=i+3){
+    t=board[i]*board[i+1]*board[i+2];
+    if(t==1)
+        winner=1;
+    if(t==8)
+        winner=2;
+  }
+  for(i=0;i<3;i++){
+    t=board[i]*board[i+3]*board[i+6];
+    if(t==1)
+          winner=1;
+    if(t==8)
+          winner=2;
+  }
+  t=board[0]*board[4]*board[8];
+  if(t==1)
+        winner=1;
+  if(t==8)
+        winner=2;
+
+  t=board[2]*board[4]*board[6];
+      if(t==1)
+            winner=1;
+      if(t==8)
+            winner=1;
+   if(winner==1)
+      alert('X is winner !');
+   if(winner==2)
+         alert('O is winner !');
+   
+
 }
